7f06577f5379e88ac2e87feab452c8ff570a2ace
GovorI <govorip@gmail.com>
2024-11-21 16:29:36 +0300

    browserRouter fix

--- src/index.js
+++ src/index.js
@@ -1,26 +1,25 @@
-import store from './redux/redux-store'
+import store from "./redux/redux-store";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import {Provider} from "react-redux";
-import {BrowserRouter} from "react-router-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 // import StoreContext from "./StoreContext";
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 function renderTree() {
-    root.render(
-        <BrowserRouter>
-            <Provider store={store}>
-                <App/>
-            </Provider>
-        </BrowserRouter>
-    );
+  root.render(
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  );
 }
 
-renderTree()
+renderTree();
 // store.subscribe(() => {
 //     renderTree()
 // })
