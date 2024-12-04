0bff7c920d9b49d3b56b274ce755d983d6696b42
Govor I <govorip@gmail.com>
2024-12-04 11:35:10 +0300

    added edit mode for profile

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -3,6 +3,9 @@ import s from "../ProfileInfo/ProfileInfo.module.css";
 import Preloader from "../../common/preloader/Preloader";
 import defaultUserAvatar from "../../../assets/images/young-man-avatar-on-round-button_icon-icons.com_68262.png";
 import ProfileStatus from "./ProfileStatus";
+import ProfileData from "./ProfileData"
+import ProfileDataForm from "./ProfileDataForm";
+
 
 function ProfileInfo(props) {
   const [edit, setEdit] = useState(false);
@@ -11,18 +14,22 @@ function ProfileInfo(props) {
     return <Preloader />;
   }
   console.log(props.profile);
-  function editPhoto() {
+  // function editPhoto() {
+  //   setEdit(true);
+  // }
+
+  function editProfile() {
     setEdit(true);
   }
 
-  const chengePhoto = (e) => {
-    const photoFile = e.target.files[0];
-    if (photoFile) {
-      console.log("changePhotoMethod" + photoFile);
-      props.savePhotoProfile(photoFile);
-      setEdit(false);
-    }
-  };
+  // const chengePhoto = (e) => {
+  //   const photoFile = e.target.files[0];
+  //   if (photoFile) {
+  //     console.log("changePhotoMethod" + photoFile);
+  //     props.savePhotoProfile(photoFile);
+  //     setEdit(false);
+  //   }
+  // };
 
   return (
     <div>
@@ -31,60 +38,23 @@ function ProfileInfo(props) {
           src={props.profile.photos.large || defaultUserAvatar}
           alt={"user`s avatar"}
           className={s.mainPhoto}
-          onClick={editPhoto}
+          // onClick={editPhoto}
         />
-        <div>
+        {/* <div>
           {props.isOwner && edit && (
             <input type="file" onChange={chengePhoto}></input>
           )}
-        </div>
+        </div> */}
       </div>
       <ProfileStatus {...props} /> <br />
-      <ProfileData profile={props.profile} />
+      {edit
+        ? <ProfileDataForm profile={props.profile} setEdit={()=>{setEdit(true)}}/>
+        : <ProfileData profile={props.profile} isOwner={props.isOwner} setEdit={()=>{editProfile()}}/>
+      }
     </div>
   );
 }
 
-const ProfileData = ({ profile }) => {
-  return (
-    <div>
-      <div>
-        <b>Full Name: </b> {profile.fullName}
-      </div>
-      <div>
-        <b>Looking for a job: </b>
-        {profile.lookingForAJob ? "yes" : "no"}
-      </div>
-      {profile.lookingForAJob && (
-        <div>
-          <b>My professional skills: </b> {profile.lookingForAJobDescription}
-        </div>
-      )}
-      <div>
-        <b>About me: </b> {profile.aboutMe}
-      </div>
-      <div>
-        <b>Contacts: </b>{" "}
-        {Object.keys(profile.contacts).map((key) => {
-          return (
-            <Contact
-              key={key}
-              contactTitle={key}
-              contactValue={profile.contacts[key]}
-            />
-          );
-        })}
-      </div>
-    </div>
-  );
-};
 
-const Contact = ({ contactTitle, contactValue }) => {
-  return (
-    <div className={s.contact}>
-      <b>{contactTitle}: </b>
-      {contactValue}
-    </div>
-  );
-};
+
 export default ProfileInfo;
